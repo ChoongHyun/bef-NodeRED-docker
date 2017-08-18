ca82af566a653b84c339980b9772310d312cd022
choong <choong@218.38.137.28>
2017-08-18 15:24:47 +0900

    good

--- app.js
+++ app.js
@@ -2,11 +2,15 @@
 var http = require('http');
 // node.js http와 connect 컴포넌트 기반의 웹 프레임워크
 var express = require('express');
+// request
 var request = require('request');
 // Create an Express app
 var app = express();
 // node-red 를 위해 package.json에 depdency 추가
 var RED = require('node-red');
+// cross-orgin
+var cors = require('cors');
+
 // Add a simple route for static content served from 'public'
 // app.use('/',express.static('public'));
 // Create a server
@@ -18,9 +22,11 @@ var settings = {
     // 실행 된 node 하위 경로
     httpNodeRoot: '/node',
     // 해당 node-red의 settings.js를 이용 경로 설정 & 저장 경로
-    userDir:'/usr/src/node-red/',
+    // userDir:'/usr/src/node-red/',
+    userDir:'/Users/choong/Downloads/',
     // custom node
-    nodesDir: '//usr/src/node-red/BEF-UI/custom/node/',
+    nodesDir: '/usr/src/node-red/BEF-UI/custom/node/',
+    nodesDir: '/Users/choong/git/bef-NodeRED-docker/BEF-UI/custom/node/',
     // admin auth
     adminAuth: require('./BEF-server/bef-admin-auth.js'),
     // category 순서
@@ -33,9 +39,11 @@ var settings = {
         page: {
             title: 'exntu',
             // css
-            css: "/usr/src/node-red/BEF-UI/custom/css/bef-ui.css",
+            // css: "/usr/src/node-red/BEF-UI/custom/css/bef-ui.css",
+            css: "/Users/choong/git/bef-NodeRED-docker/BEF-UI/custom/css/bef-ui.css",
             // UI scripts
-            scripts: '/usr/src/node-red/BEF-UI/custom/js/bef-js.js',
+            // scripts: '/usr/src/node-red/BEF-UI/custom/js/bef-js.js',
+            scripts: '/Users/choong/git/bef-NodeRED-docker/BEF-UI/custom/js/bef-js.js',
         },
         userMenu: false
     },
@@ -70,6 +78,20 @@ var settings = {
         }
     }
 };
+
+// cross-orgin
+app.use(cors());
+//var whitelist = ['http://example1.com', 'http://example2.com']
+//var corsOptions = {
+//  origin: function (origin, callback) {
+//    if (whitelist.indexOf(origin) !== -1) {
+//      callback(null, true)
+//    } else {
+//      callback(new Error('Not allowed by CORS'))
+//    }
+//  }
+//}
+
 // Initialise the runtime with a server and settings
 RED.init(server,settings);
 // serve the http : admin
@@ -77,7 +99,7 @@ app.use(settings.httpAdminRoot,RED.httpAdmin);
 // serve the http : nodes
 app.use(settings.httpNodeRoot,RED.httpNode);
 // port
-server.listen(1880);
+server.listen(8080);
 // Start the runtime
 // node-red document 가면 RED runtime api 참고
 RED.start().then(function(){
@@ -89,7 +111,7 @@ RED.start().then(function(){
 // 404 page error 페이지로 넘기는 처리 방법
 app.get('*', function(req,res){
     res.status(404);
-    res.sendFile('/usr/src/node-red/HTML/404page.html');
+    res.sendFile('/Users/choong/git/bef-NodeRED-docker/HTML/404page.html');
     // res.redirect('/error');
     // res.send('error');
     // res.end();
@@ -97,5 +119,5 @@ app.get('*', function(req,res){
 // 에러 페이지
 app.get('/error', function(req,res){
     res.status(500);
-    res.sendFile('/usr/src/node-red/HTML/500page.html');
+    res.sendFile('/Users/choong/git/bef-NodeRED-docker/HTML/500page.html');
 });
